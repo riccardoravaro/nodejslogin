ef7e476e1464682e3ec5a4960a61b2f14f11d385
riccardo <ravaro.riccardo@gmail.com>
2016-10-23 20:12:38 +0100

    registration code and login code completed

--- app.js
+++ app.js
@@ -4,21 +4,20 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var passport = require('passport');
-var expressValidator = require('express-validator');
-var LocalStrategy = require('passport-local').Strategy;
-var multer = require('multer');
-//handle file uploads
-var upload = multer({dest:'./uploads'});
-var flash = require('connect-flash');
 var session = require('express-session');
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var mongodb = require('mongodb');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var expressValidator = require('express-validator');
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
+var flash = require('connect-flash');
+var bcrypt = require('bcryptjs');
+var mongo = require('mongodb');
 var mongoose = require('mongoose');
 var db = mongoose.connection;
 
-
+var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -26,27 +25,20 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-
-
-
-
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-// handle session
+// Handle Sessions
 app.use(session({
   secret:'secret',
   saveUninitialized: true,
   resave: true
-}))
+}));
 
-// passwport middleware
+// Passport
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -68,10 +60,17 @@ app.use(expressValidator({
   }
 }));
 
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(require('connect-flash')());
+app.use(flash());
 app.use(function (req, res, next) {
   res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+app.get('*', function(req, res, next){
+  res.locals.user = req.user || null;
   next();
 });
 
